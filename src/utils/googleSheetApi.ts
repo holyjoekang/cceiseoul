c039f3524660f0caa37f85bb323b690275878b9d
holyjoekang <146902251+holyjoekang@users.noreply.github.com>
2026-08-05 06:47:38 +0900

    Update googleSheetApi.ts

--- src/utils/googleSheetApi.ts
+++ src/utils/googleSheetApi.ts
@@ -1,14 +1,15 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbxKyiN5TAI8l59yqGLoZfVTdQZ8Ty_lCdII9U6MlGGDZJbrH1zNqahYWMn3M3WzkdMHUQ/exec";
 
 
-export async function getReservations() {
-
-  const response = await fetch(API_URL);
-
-  const data = await response.json();
-
-  return data;
-
+export async function createReservation(reservation: any) {
+  const response = await fetch(API_URL, {
+    method: "POST",
+    body: JSON.stringify({
+      action: "create",
+      reservation: reservation,
+    }),
+  });
+  return await response.json();
 }
 
 
